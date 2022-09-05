772376973241c8d08912150e5a951ca9b91c0a5d
André Masson <andre.masson@lacapitale.com>
2022-09-05 07:08:42 -0400

    chore: initial revision

---
+++ android/app/src/main/kotlin/com/example/poc_flutter_02_bloc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.poc_flutter_02_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'ui/movie_list.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        body: MovieList(),
+      ),
+    );
+  }
+}
